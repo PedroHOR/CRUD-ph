2a70318446fc949ae4ddca3928d7aaebe4b8dccd
PedroHOR <phorx2017@yahoo.com>
2021-11-02 17:32:39 -0300

    Primeiro commit
    Primeiro repositorio

---
+++ index.js
@@ -0,0 +1,30 @@
+var dados = []
+
+function PopulaTabela() {
+    if (Array.isArray(dados)) {
+
+        $("#tblDados tbody").html("")
+
+        dados.forEach(function (item) {
+            $("#tblDados tbody").append(`<tr>
+            <td>${item.ID}</td>
+            <td>${item.Nome}</td>
+            <td>${item.Sobrenome}</td>
+            <td>${item.DtNascimento}</td>
+            <td>${item.Formacao}</td>
+            </tr>`)
+
+        })
+    }
+}
+
+
+$(function () {
+    
+    dados = JSON.parse(localStorage.getItem("__dados__"))
+
+    if (dados) {
+        PopulaTabela()
+    }
+
+})
