627d961167537e40f75ac8cd8506883da33d84fa
PedroHOR <phorx2017@yahoo.com>
2021-11-08 10:38:14 -0300

    implementos
    implementação no JS

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var dados = []
 function PopulaTabela() {
     if (Array.isArray(dados)) {
 
+        localStorage.setItem("__dados__", JSON.stringify(dados))
+
         $("#tblDados tbody").html("")
 
         dados.forEach(function (item) {
@@ -20,11 +22,40 @@ function PopulaTabela() {
 
 
 $(function () {
-    
     dados = JSON.parse(localStorage.getItem("__dados__"))
 
     if (dados) {
         PopulaTabela()
     }
+
+    $("btnSalvar").click(function () {
+
+        let nome = $("txtNome").val()
+        let sobrenome = $("txtSobrenome").val()
+        let dtNascimento = new Date($("#txtDtNascimento").val()).toLocaleDateString("pt-br", {timeZone: "UTC" })
+        let formacao = $("txtFormacao").val()
+
+        let registro = {}
+
+        registro.Nome = Nome
+        registro.Sobrenome = Sobrenome
+        registro.DtNascimento = DtNascimento
+        registro.Formacao = Formacao
+
+        registro.ID = dados.length + 1
+
+        dados.push(registro)
+
+        alert("Reistro salvo")
+        $("#modalRegistro").modal("hide")
+
+        $("#txtNome").val("")
+        $("#txtSobrenome").val("")
+        $("#txtDtNascimento").val("")
+        $("#txtFormacao").val("")
+
+        PopulaTabela()
+    })
+
 
 })
